5e09b42cffa49ebd85ea6b855b96a3430dacc86a
AlonaAkimova <aliona.akimova@gmail.com>
2024-02-14 18:41:53 +0700

    reducing text size

--- molo-residence-react/components/BreakfastItem.tsx
+++ molo-residence-react/components/BreakfastItem.tsx
@@ -19,7 +19,7 @@ const BreakfastItem: FC<BreakfastItemProps> = ({ breakfast, onClick }) => {
   return (
     <li key={breakfast.id} className="mb-6">
       <div className="flex flex-col items-center">
-        <h3 className="text-sm font-semibold mb-2">{breakfast.name}</h3>
+        <h3 className="text-neutral-950 font-bold mb-2">{breakfast.name}</h3>
         <p className="text-gray-700">{breakfast.description}</p>
         <Button
           role="button"
