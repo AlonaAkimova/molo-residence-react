2be921ddd1f6ea54b4ec09bd137fc1be7a342d9d
AlonaAkimova <aliona.akimova@gmail.com>
2024-02-14 17:41:22 +0700

    changes

--- molo-residence-react/app/layout.tsx
+++ molo-residence-react/app/layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import "./globals.css";
+import type { Viewport } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { BreakfastOrderProvider } from "@/store/BreakfastOrderProvider";
 import Header from "@/components/Header";
@@ -17,7 +18,16 @@ export const metadata: Metadata = {
   title: "MoloResidence",
   description: "MoloResidence breakfast",
 };
-
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "cyan" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  colorScheme: "light",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
