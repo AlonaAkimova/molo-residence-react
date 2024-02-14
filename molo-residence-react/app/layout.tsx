643ab9c3a9165532056e3a600d636e177c86671e
AlonaAkimova <aliona.akimova@gmail.com>
2024-02-14 16:52:49 +0700

    change

--- molo-residence-react/app/layout.tsx
+++ molo-residence-react/app/layout.tsx
@@ -3,8 +3,7 @@ import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { BreakfastOrderProvider } from "@/store/BreakfastOrderProvider";
 import Header from "@/components/Header";
-import { Rubik } from "next/font/google";
-import { ThemeProvider } from "@mui/material/styles";
+
 interface RootLayoutProps {
   children: ReactNode;
 }
@@ -14,10 +13,6 @@ interface Metadata {
   description: string;
 }
 
-const rubik = Rubik({
-  subsets: ["latin"],
-  weight: "400",
-});
 export const metadata: Metadata = {
   title: "MoloResidence",
   description: "MoloResidence breakfast",
