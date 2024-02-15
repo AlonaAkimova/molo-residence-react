1bde1d4618e1affd2ed90a173a260f448bda2733
AlonaAkimova <aliona.akimova@gmail.com>
2024-02-15 13:50:42 +0700

    changing buttons styling

--- molo-residence-react/app/guest-list/page.tsx
+++ molo-residence-react/app/guest-list/page.tsx
@@ -33,14 +33,14 @@ const GuestNumber: FC = () => {
           <div className="flex items-center justify-center">
             <Button
               onClick={handleDecrease}
-              className="w-8 h-8 flex items-center justify-center rounded-full border border-gray-300 ml-2 font-bold"
+              className="w-8 h-8 flex items-center text-neutral-950 justify-center rounded-full border border-gray-300 ml-2 font-bold"
             >
               -
             </Button>
             <p className="text-neutral-950 font-bold">{numberOfGuests}</p>
             <Button
               onClick={handleIncrease}
-              className="w-8 h-8 flex items-center justify-center rounded-full border border-gray-300 ml-2 font-bold"
+              className="w-8 h-8 flex items-center text-neutral-950 justify-center rounded-full border border-gray-300 ml-2 font-bold"
             >
               +
             </Button>
